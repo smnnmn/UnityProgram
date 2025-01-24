5b314e3225a34d32b6122d2d3ac2691dc6ea41e2
smnnmn <leejs899797@gmail.com>
2025-01-24 21:43:31 +0900

    {2025.1.24} 사운드 시스템

--- Program/Assets/Class9th (Collision)/Scripts/BilliardBall.cs
+++ Program/Assets/Class9th (Collision)/Scripts/BilliardBall.cs
@@ -4,6 +4,45 @@ using UnityEngine;
 
 public class BilliardBall : MonoBehaviour
 {
+    private Rigidbody rigidbody;
+    [SerializeField] Vector3 direction;
+
+    private void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        direction.x = Input.GetAxisRaw("Horizontal");
+        direction.z = Input.GetAxisRaw("Vertical");
+        direction.Normalize();
+
+    }
+    private void FixedUpdate()
+    {
+        rigidbody.AddForce(direction, ForceMode.Impulse);
+
+        // ForceMode.Force : 지속적인 힘을 적용하는 방식이며,
+        // 객체에 힘을 계속적으로 전달하는
+        // 방법으로 시간에 비례하여 적용합니다.
+
+        // ForceMode.Impulse : 순간적인 힘을 적용하는 방식이며,
+        // 객체의 가속도가 아닌 속도를 변화시키는 방식입니다.
+
+        // ForceMode.VelocityChange : 순간적인 힘을 적용하는 방식이며,
+        // 객체의 속도만 변화시키며 객체의 무게에는
+        // 영향을 받지 않습니다.
+
+        // ForceMode.Acceleration " 연속적인 힘을 적용하는 방식이며,
+        // 객체에 힘을 계속적으로 전달하는 방법이지만,
+        // 객체의 무게에 받지 않습니다.
+    }
     private void OnCollisionEnter(Collision collision)
     {
         
@@ -18,5 +57,25 @@ public class BilliardBall : MonoBehaviour
         Debug.Log("OnCollisionExit");
     }
 
-
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Goal Line")
+        {
+            Debug.Log("OnTriggerEnter");
+        }
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Goal Line")
+        {
+            Debug.Log("OnTriggerStay");
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Goal Line")
+        {
+            Debug.Log("OnTriggerExit");
+        }
+    }
 }
